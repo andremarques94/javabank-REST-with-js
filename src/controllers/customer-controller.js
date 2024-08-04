529e65793e8c81244c449083d0eb4b7d9ce590a0
NOZK <90326088+andremarques94@users.noreply.github.com>
2024-08-04 12:51:10 +0100

    Adds error handling

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -25,21 +25,49 @@ export async function getCustomer(req, res) {
 }
 
 export async function addCustomer(req, res) {
-  const customer = req.body;
-  const data = await customerService.addCustomer(customer);
+  if (!req.body) {
+    res.status(400).send("Customer data is required");
+    return;
+  }
+
+  const data = await customerService.addCustomer(req.body);
+
+  if (!data) {
+    res.status(500).send("Error adding customer");
+    return;
+  }
+
   res.status(201);
   res.send(data);
 }
 
 export async function updateCustomer(req, res) {
-  const customer = req.body;
-  const data = await customerService.updateCustomer(customer);
+  if (!req.body) {
+    res.status(400).send("Customer data is required");
+    return;
+  }
+
+  const data = await customerService.updateCustomer(req.body);
+
+  if (!data) {
+    res.status(500).send("Error updating customer");
+    return;
+  }
+
   res.status(200);
   res.send(data);
 }
 
 export async function deleteCustomer(req, res) {
+  if (!req.params.id) res.status(400).send("Customer ID is required");
+
   const deleted = customerService.deleteCustomer(req.params.id);
+
+  if (!deleted) {
+    res.status(500).send("Error deleting customer");
+    return;
+  }
+
   res.status(200);
   res.send(deleted);
 }
