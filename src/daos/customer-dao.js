a8e76ac582614ee77f234bac46a3b78e5872fde7
NOZK <90326088+andremarques94@users.noreply.github.com>
2024-08-04 13:32:39 +0100

    Adds transaction handling to service

--- src/daos/customer-dao.js
+++ src/daos/customer-dao.js
@@ -22,25 +22,7 @@ export function create(customer) {
 }
 
 export function update({ id, ...customer }) {
-  return Customer.transaction(async (trx) => {
-    const customerToUpdate = await Customer.query(trx).findById(id);
-
-    if (!customerToUpdate) {
-      throw new Error("Customer not found");
-    }
-
-    const data = {
-      ...customerToUpdate,
-      ...Object.keys(customer)
-        .filter((key) => !(customer[key] === ""))
-        .reduce((acc, key) => {
-          acc[key] = customer[key];
-          return acc;
-        }, {}),
-    };
-
-    return Customer.query(trx).updateAndFetchById(id, data);
-  });
+  return Customer.query().patchAndFetchById(id, customer);
 }
 
 export function remove(id) {
--- src/services/customer-service.js
+++ src/services/customer-service.js
@@ -1,4 +1,6 @@
 import { list, get, create, remove, update } from "../daos/customer-dao.js";
+import { transaction } from "objection";
+import Customer from "../models/customer.js";
 
 export async function listCustomers() {
   return list();
@@ -9,11 +11,42 @@ export async function getCustomerById(id) {
 }
 
 export async function addCustomer(customer) {
-  return create(customer);
+  return transaction(Customer.knex(), async (trx) => {
+    try {
+      return create(customer).transacting(trx);
+    } catch (error) {
+      trx.rollback();
+      throw error;
+    }
+  });
 }
 
-export async function updateCustomer(id, customer) {
-  return update(id, customer);
+export async function updateCustomer(customer) {
+  return transaction(Customer.knex(), async (trx) => {
+    try {
+      const customerToUpdate = await get(customer.id);
+
+      if (!customerToUpdate) {
+        throw new Error("Customer not found");
+      }
+
+      const data = {
+        ...customerToUpdate,
+        ...Object.keys(customer).reduce((acc, key) => {
+          if (customer[key] && customer[key] !== "") {
+            acc[key] = customer[key];
+          }
+
+          return acc;
+        }, {}),
+      };
+
+      return update(data).transacting(trx);
+    } catch (error) {
+      trx.rollback();
+      throw error;
+    }
+  });
 }
 
 export async function deleteCustomer(id) {
