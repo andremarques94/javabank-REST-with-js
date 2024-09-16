d461fa0e48fc75779f6a32afaeedc6ea7e88e547
NOZK <90326088+andremarques94@users.noreply.github.com>
2024-09-16 11:24:56 +0100

    Adds comment to implementation index.js file

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import cors from "cors";
 const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
 
-app.use(cors());
-app.use(json());
+app.use(cors()); //Middleware necessary for cross-origin requests
+app.use(json()); //Middleware necessary for reading request body
 
 app.use("/api", customerRouter);
 app.use("/api", accountRouter);
 
+// Redirect to /api/customer
+// If the user accesses the root of the application, it will be redirected to /api/customer
 app.use("/", (_, res) => {
   res.redirect("api/customer");
 });
